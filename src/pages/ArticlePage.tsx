e950957c27e874c8174781384a1c86426206a441
artur <arturandros09@gmail.com>
2023-09-02 20:30:39 +0200

    update app with jotai store

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import { useAtom } from "jotai";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { articleAtom } from "../store/stroe";
 
 const ArticlePage = () => {
+  const [article] = useAtom(articleAtom);
   const { id } = useParams();
+
   return (
     <div>
       <h2>ArticlePage</h2>
       <p>ID: {id}</p>
+      {article ? (
+        <div>
+          <h4>{article?.title}</h4>
+          <p>{article?.description}</p>
+        </div>
+      ) : (
+        <p>No content</p>
+      )}
     </div>
   );
 };
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getNews } from "../service/api";
+import { NewsInterface } from "../types/types";
+import { useNavigate } from "react-router-dom";
+import { useAtom } from "jotai";
+import { articleAtom } from "../store/stroe";
 
 const MainPage = () => {
-  return <h1>MainPage</h1>;
+  const [news, setNews] = useState<NewsInterface[] | []>([]);
+  const [, setArticle] = useAtom(articleAtom);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await getNews();
+    setNews(data.results);
+  };
+
+  const handleArticleClick = (elem: NewsInterface) => {
+    setArticle(elem);
+    navigate(`${elem.article_id}`);
+  };
+
+  return (
+    <div>
+      <h1>MainPage</h1>
+      {news.length ? (
+        <div>
+          <h2> some news</h2>
+          {news.map((element) => (
+            <div
+              key={element?.article_id}
+              onClick={() => handleArticleClick(element)}
+            >
+              <h4>{element?.title}</h4>
+              <p>{element?.description}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <h2>No news</h2>
+      )}
+    </div>
+  );
 };
 
 export default MainPage;
